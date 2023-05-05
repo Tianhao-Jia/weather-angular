8c818248ba96d3c5253f4f1491d9a1639e4b9d42
Tom Jia <tjia@fortinet-us.com>
2023-05-05 16:32:51 -0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component,OnDestroy,OnInit } from '@angular/core';
+import { Input } from '@angular/core';
 
+import { FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit,OnDestroy {
   title = 'my-project';
+  cities = ["London", "Paris", "Moscow", "New York", "Karachi", "Sydney"];
+  
+  cityControl!: FormControl;
+
+
+  constructor(private router: Router) { }
+  ngOnInit(): void {
+    this.cityControl = new FormControl("");
+    this.cityControl.valueChanges.subscribe((value) => {
+      this.router.navigate([value]);
+    });
+  }
+  ngOnDestroy(): void {
+    
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSelectModule} from '@angular/material/select';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSlideToggleModule,
+    MatToolbarModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    ReactiveFormsModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+
+  
+}
